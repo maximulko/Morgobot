6c7d781f43b08447d8206e96d148eac9c8ece035
Anton Ilin <anton_ilin@symbotic.com>
2017-05-22 17:32:12 +0300

    DI

--- Morgobot/Program.cs
+++ Morgobot/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Threading.Tasks;
-using Telegram.Bot;
 
 namespace Morgobot
 {
@@ -14,39 +13,21 @@ namespace Morgobot
 
         static async Task MainAsync(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Listening new messages");
+            var serviceProvider = CreateServiceProvider();
 
-            var settingsManager = new SettingsManager();
-            var brain = new Brain();
+            var server = serviceProvider.GetService<Server>();
+            await server.Run();
+        }
 
+        static IServiceProvider CreateServiceProvider()
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<Brain>()
+                .AddSingleton<Server>()
+                .AddSingleton<SettingsManager>()
+                .BuildServiceProvider();
 
-            var bot = new TelegramBotClient(settingsManager.GetSetting("botToken"));
-            var offset = 0;
-
-            while (true)
-            {
-                var updates = await bot.GetUpdatesAsync(offset);
-
-                if (updates.Any())
-                {
-                    foreach(var update in updates)
-                    {
-                        Console.WriteLine($"Incoming message from {update.Message.From.FirstName} {update.Message.From.LastName}: {update.Message.Text}");
-
-                        var charId = update.Message.Chat.Id;
-                        var reply = brain.Analyse(update.Message.Text);
-                        var message = await bot.SendTextMessageAsync(charId, reply);
-                        offset = update.Id + 1;
-                    }
-                }
-                else
-                {
-                    //Console.WriteLine("No updates");
-                }
-
-                await Task.Delay(1000);
-            }
+            return serviceProvider;
         }
     }
 }
---
+++ Morgobot/Server.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Morgobot
+{
+    public class Server
+    {
+        private Brain _brain;
+        private SettingsManager _settingsManager;
+
+        public Server(Brain brain, SettingsManager settingsManager)
+        {
+            _brain = brain;
+            _settingsManager = settingsManager;
+        }
+
+        public async Task Run()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Listening new messages");
+
+            var bot = new TelegramBotClient(_settingsManager.GetSetting("botToken"));
+            var offset = 0;
+
+            while (true)
+            {
+                var updates = await bot.GetUpdatesAsync(offset);
+
+                if (updates.Any())
+                {
+                    foreach (var update in updates)
+                    {
+                        Console.WriteLine($"Incoming message from {update.Message.From.FirstName} {update.Message.From.LastName}: {update.Message.Text}");
+
+                        var charId = update.Message.Chat.Id;
+                        var reply = _brain.Analyse(update.Message.Text);
+                        var message = await bot.SendTextMessageAsync(charId, reply);
+                        offset = update.Id + 1;
+                    }
+                }
+                else
+                {
+                    //Console.WriteLine("No updates");
+                }
+
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
