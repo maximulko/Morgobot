fdb7d1ab38b989b8f9b847478d61f5d4d795e0bf
Anton Ilin <anton_ilin@symbotic.com>
2017-05-25 12:35:14 +0300

    BrainTest.cs

---
+++ Tests/BrainTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Morgobot.Brain;
+using Moq;
+using Morgobot.Brain.Movements;
+
+namespace Tests
+{
+    [TestClass]
+    public class BrainTest
+    {
+        private Brain _brain;
+        private Mock<BasicThoughts> _basicThoughtsMock;
+        private Mock<MovementThoughts> _movementThoughtsMock;
+        private Mock<Huefication> _hueficationMock;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _basicThoughtsMock = new Mock<BasicThoughts>();
+            //_basicThoughtsMock.Setup(bt => bt.Analyse(It.IsAny<Phrase>())).Returns("Пожалуйста");
+
+
+            _movementThoughtsMock = new Mock<MovementThoughts>();
+            _hueficationMock = new Mock<Huefication>();
+
+            _brain = new Brain(_basicThoughtsMock.Object, _movementThoughtsMock.Object, _hueficationMock.Object);
+        }
+
+        [TestMethod]
+        public void BasicThoughtsTest()
+        {
+            //_brain.Analyse("спасибо", 0);
+            //_basicThoughtsMock.Verify(bt=>bt.Analyse(It.IsAny<Phrase>()));
+        }
+    }
+}
