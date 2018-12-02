f645567fb646496d801078e2a72b881f9040f1e1
Anton Ilin <anton_ilin1@epam.com>
2018-12-02 11:26:02 +0200

    WebhookController.cs

---
+++ app/Morgobot.Web/WebhookController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Morgobot.Web
+{
+    [Route("[controller]")]
+    public class WebhookController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
