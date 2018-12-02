49293f66455b63fbc42ce58fd1d8bd3a9bdb3b47
Anton Ilin <anton_ilin1@epam.com>
2018-12-02 12:17:50 +0200

    Logging

--- app/Morgobot.Web/WebhookController.cs
+++ app/Morgobot.Web/WebhookController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -12,18 +13,28 @@ namespace Morgobot.Web
     {
         private readonly Brain.Brain _brain;
         private readonly TelegramBotClient _client;
+        private readonly ILogger<WebhookController> _logger;
 
-        public WebhookController(Brain.Brain brain, TelegramBotClient client)
+        public WebhookController(
+            Brain.Brain brain, 
+            TelegramBotClient client,
+            ILogger<WebhookController> logger)
         {
             _brain = brain ?? throw new System.ArgumentNullException(nameof(brain));
             _client = client ?? throw new System.ArgumentNullException(nameof(client));
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
         }
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody]Update update)
         {
-            if (update == null)
+            if (update == null || update.Message == null)
+            {
+                _logger.LogWarning("Incoming null message.");
                 return BadRequest();
+            }
+
+            _logger.LogInformation($"Incoming message from {update.Message.From.FirstName} {update.Message.From.LastName} ({update.Message.From.Id}): {update.Message.Text}");
 
             var chatId = update.Message.Chat.Id;
             var reply = _brain.Analyse(update);
