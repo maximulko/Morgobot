5c1636f3eb9061437115b7a9ffa4d5f7d87fbc57
Anton Ilin <anton_ilin1@epam.com>
2018-12-04 23:26:08 +0200

    Debug webhook

---
+++ app/Morgobot.Web/Controllers/DebugController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Morgobot.Web.Controllers
+{
+    [Route("[controller]")]
+    public class DebugController : Controller
+    {
+        private readonly TelegramBotClient _telegramBotClient;
+
+        public DebugController(TelegramBotClient telegramBotClient)
+        {
+            _telegramBotClient = telegramBotClient ?? throw new ArgumentNullException(nameof(telegramBotClient));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var webhookInfo = await _telegramBotClient.GetWebhookInfoAsync();
+            return Ok(webhookInfo);
+        }
+    }
+}
